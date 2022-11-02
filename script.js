5a4335ef512600daf8ce8c3e5cce3133a3825d8d
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2022-11-02 00:37:01 -0400

    created rockPaperScissors function and content

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ let Paper = 2
 let Scissors = 3
 let options = ['Rock', 'Paper', 'Scissors']
 
-let getComputerChoice = function(options) {
+function getComputerChoice(options) {
     return options[(Math.floor(Math.random() * options.length))];
 };
-console.log(getComputerChoice(options))
+console.log(getComputerChoice(options));
+const computerSelection = getComputerChoice(options);
 
-const rps0 = prompt("Choose Rock, Paper, or Scissors:");
-console.log(rps0);
-let rps1 = rps0.replace((rps0), (rps0.charAt(0).toUpperCase() + rps0.slice(1).toLowerCase()));
-console.log(rps1);
 
+const playerReply = prompt("Choose Rock, Paper, or Scissors:");
+console.log(playerReply);
+const playerSelection = playerReply.replace((playerReply), (playerReply.charAt(0).toUpperCase() + playerReply.slice(1).toLowerCase()));
+console.log(playerSelection);
+
+
+function rockPaperScissors(playerSelection, computerSelection) {
+    if ((playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
+        console.log(`${computerSelection} beats ${playerSelection}. You lose!`);
+    } else {
+        console.log(`${playerSelection} beats ${computerSelection}. You win!`)
+    }
+};
+console.log(rockPaperScissors(playerSelection, computerSelection));
