a920d5448705866b410c1620690d3e67358dbc25
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2022-11-01 23:54:15 -0400

    Created prompt, formatted prompt.

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let Rock = 1
 let Paper = 2
 let Scissors = 3
-const options = ['Rock', 'Paper', 'Scissors']
+let options = ['Rock', 'Paper', 'Scissors']
 
-let getComputerChoice = function() {
+let getComputerChoice = function(options) {
     return options[(Math.floor(Math.random() * options.length))];
 };
+console.log(getComputerChoice(options))
+
+const rps0 = prompt("Choose Rock, Paper, or Scissors:");
+console.log(rps0);
+let rps1 = rps0.replace((rps0), (rps0.charAt(0).toUpperCase() + rps0.slice(1).toLowerCase()));
+console.log(rps1);
+
