e24418a61cde7578a65eb67d6d25eaf501b177cf
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2022-11-02 14:31:51 -0400

    coded game function, need scorekeeping

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 
-let getComputer = function() {
-    let options = ['Rock', 'Paper', 'Scissors']
-    return options[(Math.floor(Math.random() * options.length))];
-};
+function game() {
+    const playerScore = 0;
+    const computerScore = 0;
 
-let getPlayer = function() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayer()
+        const computerSelection = getComputer()
+
+        function playRound() {
+
+            if ((playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
+                console.log(`${computerSelection} beats ${playerSelection}. You lose!`);
+                computerScore++;
+            } else if (playerSelection == computerSelection) {
+                console.log(`Tie! Try again.`);
+            } else {
+                console.log(`${playerSelection} beats ${computerSelection}. You win!`);
+                playerScore++;
+            };
+        };
+
+    };
+
+}
+
+function getPlayer() {
     let playerReply = prompt("Choose Rock, Paper, or Scissors:");
     let playerSelection = playerReply.replace((playerReply), (playerReply.charAt(0).toUpperCase() + playerReply.slice(1).toLowerCase()));
     return playerSelection;
-};
-
-let playRound = function() {
-    let playerSelection = getPlayer();
-    let computerSelection = getComputer();
-
-
-    if ((playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
-        console.log(`${computerSelection} beats ${playerSelection}. You lose!`);
-    } else if ( playerSelection == computerSelection) {
-        console.log(`Tie! Try again.`)
-    } else {
-        console.log(`${playerSelection} beats ${computerSelection}. You win!`)
-    }
-};
-
-let game = function() {
-    Create Counters 
-    loop
-    nest playRound inside of here
-    add Counters
-    add returns
-    
 }
+    
+
+function getComputer() {
+    let options = ['Rock', 'Paper', 'Scissors'];
+    return options[(Math.floor(Math.random() * options.length))];
+}
+
+
