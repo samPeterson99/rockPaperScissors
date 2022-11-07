e92ea10d28426d2579a2d4cf49411736bd18007a
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2022-11-06 22:31:52 -0500

    finished ui

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-//generates a random computer answer
 function getComputer() {
     let options = ['Rock', 'Paper', 'Scissors'];
     return options[(Math.floor(Math.random() * options.length))];
 }
 
-//prompts player for answer and formats
 function getPlayer() {
     let playerReply = prompt("Choose Rock, Paper, or Scissors:");
     let playerSelection = playerReply.replace((playerReply), (playerReply.charAt(0).toUpperCase() + playerReply.slice(1).toLowerCase()));
     return playerSelection;
 }
 
-//creates scoreboard
 let playerScore = 0;
 let computerScore = 0;
 
-//code for one iteration of the game.
-//personal note: code acts because it pulls new Selections, alters score.
 function playRound(selection) {
     let playerSelection = selection;
     let computerSelection = getComputer()
     console.log(playerSelection);
     console.log(computerSelection);
 
-
     // includes scorekeeper for game()
     if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         computerScore++;
-        console.log(computerScore);
         winner.textContent = (`${computerSelection} beats ${playerSelection}. You lose!`);
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
         computerScore++;
@@ -42,12 +35,10 @@ function playRound(selection) {
         winner.textContent = (`${playerSelection} beats ${computerSelection}. You win!`);
     };
     bigWinner.textContent = `${finale(playerScore, computerScore)}`
-    return score.textContent = `Player: ${playerScore}    Computer:${computerScore}`
+    score.textContent = `Player: ${playerScore}    Computer:${computerScore}`
 };
 
-
-
-
+//button
 const bbox = document.querySelector('.bbox')
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
@@ -65,9 +56,8 @@ scissors.addEventListener('click', () => {
     console.log('Scissors');
     playRound('Scissors');
 });
-
+//DOM
 const data = document.querySelector('.data');
-
 const winner = document.querySelector('.winner');
 winner.textContent = 'Play a game!';
 winner.classList.add('winner')
@@ -80,43 +70,13 @@ const bigWinner = document.querySelector('.bigWinner')
 bigWinner.classList.add('.bigWinner')
 bigWinner.textContent = `${finale(playerScore, computerScore)}`
 
-//data.appendChild(winner);
-//data.appendChild(score);
-
 function finale(playerScore, computerScore) {
     
     if (playerScore >= 5) {
-        return`Humanity wins! ${playerScore} to ${computerScore}`
+        return`Humanity wins!\n${playerScore} to ${computerScore}`
    } else if (computerScore >= 5) {
-        return `Humanity loses. ${computerScore} to ${playerScore}. Perdition beckons.`
+        return `Humanity loses.\n${computerScore} to ${playerScore}.`
    } else {
         return  ``
     } 
-}
-
-
-// very simple loop, will try to rewrite loop in better way. provides final score
-// personal note: code acts because it replays a working function then uses its output.
-/*function game() {
-//    //reset scores
-
-
-    score.textContent = `${playerScore}-${computerScore}`;
-
-    for (let i = 0; i < 5; i++) {
-        playRound() 
-        };
-    function nameWinner(){
-        if (playerScore > computerScore) {
-             score.textContent = `Humanity wins! ${playerScore} to ${computerScore}`
-        } else if (playerScore < computerScore) {
-            score.textContent = `Humanity loses. ${computerScore} to ${playerScore}. Perdition beckons.`
-        } else {
-            score.textContent = `Tie game! The score was ${computerScore} to ${playerScore}. Maybe we can do that thing from the movie War Games.`
-        }
-        } 
-         }; */
-
-
-
-    //game works, display does not
+};
